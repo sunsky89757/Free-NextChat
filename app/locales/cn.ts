6226d930898dc83bda92f1f4fcce954cf771ad1f
sunsky89757 <sunsky20120101@163.com>
2025-03-17 15:26:16 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -616,7 +616,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "有什么可以帮你的吗",
+    BotHello: "有什么可以帮你的吗，欢迎加入公益AI QQ交流群：650981028",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
